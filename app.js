1a125341b4017f93c26c87cbbdb5e088b57ba574
Phuc <phucgo240699@gmail.com>
2020-05-28 20:52:48 +0700

    Fix bug syntax

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function authenticateToken(req, res, next) {
       if (err) {
         return res.json({ success: false, error: err });
       }
-      req.username = username;
+      req.username = decoded;
       next();
     }
   );
