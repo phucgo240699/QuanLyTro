4f9c83c3ee3c9526107ab9f1de3be666c55fdcc0
Phuc <phucgo240699@gmail.com>
2020-06-27 14:50:02 +0700

    Fix return error  in authenticationToken

--- services/authenticateToken.js
+++ services/authenticateToken.js
@@ -7,8 +7,9 @@ const authenticateToken = async (req, res, next) => {
     "abdccf7cf5a25fb44f8cba244d42123285ce9207fac4db009e7b22423a17133f8ebc554537b64de47c8668ab9566c2078d4239b7e57be722edc887a3ad2bc40f",
     async function (err, decoded) {
       if (err) {
-        return res.json({ success: false, error: err });
+        return res.json({ success: false, error: err.message });
       }
+
       const user = await mongoose
         .model("users")
         .findOne({ username: decoded.username, isDeleted: false });
