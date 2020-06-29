816a7960656d3bd7e5beb12b28b6745ff8efe959
Phuc <phucgo240699@gmail.com>
2020-06-29 19:46:22 +0700

    Fix bug check token

--- services/checkToken.js
+++ services/checkToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const checkToken = async (req, res, next) => {
   const username = jwt.verify(
     req.headers.authorization,
-    process.env.SECRET_KEY.toString(),
+    "abdccf7cf5a25fb44f8cba244d42123285ce9207fac4db009e7b22423a17133f8ebc554537b64de47c8668ab9566c2078d4239b7e57be722edc887a3ad2bc40f",
     async function (err, decoded) {
       if (err) {
         return res.json({ success: false, error: err.message });
